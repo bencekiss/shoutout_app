50a23ca46c34ca49ffa8cde2b91bbe1a92d70a1f
Dgibbins <nayr_ryan@hotmail.com>
2017-02-24 18:29:30 -0500

    RD- New Restaurant Form updated with nested attribute

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,7 +12,7 @@ class RestaurantsController < ApplicationController
     if @restaurant.save
       redirect_to root_path
     else
-      render: 'new'
+      render 'new'
     end
   end
 
--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,22 @@
 class RewardsController < ApplicationController
+
+  def new
+    #code
+  end
+
+  def edit
+
+  end
+
+  def create
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def destroy
+    #code
+  end
 end
