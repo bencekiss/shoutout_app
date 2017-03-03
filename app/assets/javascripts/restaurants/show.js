0f57e0db29f22c237ce756509ec7e7e55ace98a2
Dgibbins <nayr_ryan@hotmail.com>
2017-03-02 19:31:39 -0500

    Ajax Problem, redirect to show page

---
+++ app/assets/javascripts/restaurants/show.js
@@ -0,0 +1,31 @@
+$(document).on("turbolinks:load",function(){
+  $(".reward-button").on('click',function(e){
+    $(this).children(".reward-modal").fadeIn("slow");
+  });
+  $(".close").on("click",function(e){
+    e.stopPropagation();
+    $(".reward-modal").fadeOut("slow");
+  });
+  $(".redeem-button").on('click', function(e){
+    e.stopPropagation();
+    var user_id = $(".hidden-user").val();
+    var reward_id = $(".hidden-reward").val();
+    $.ajax({
+      url:"/users/"+ user_id,
+      method:"POST",
+      data:{
+        "reward_id": reward_id,
+        "user_id": user_id,
+        "id": user_id
+      },
+      dataType:"html"
+
+    }).done(function(data){
+
+      console.log(data);
+
+    }).fail(function(){
+      console.log("Failed");
+    });
+  });
+});
