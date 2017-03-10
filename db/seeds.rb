1a2980f82bbacc8d3ce0de3b78121cde83347f4c
Bence Kiss <kiss.bence.balint@gmail.com>
2017-03-09 20:51:13 -0500

    Added seeds and modified views from rest_image to rest_img

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Restaurant.destroy_all
-# Reward.destroy_all
+Restaurant.destroy_all
+Reward.destroy_all
 # User.destroy_all
 # Shout.destroy_all
 
@@ -27,14 +27,75 @@
   #   rest_image: "http://www.shawarmasking.ca/wp-content/uploads/2016/08/Shawarma-King-Logo2.png",
   #   owner_id: 1
   # )
-  #
-  # Restaurant.create!(
-  #   name: "Shawarma King",
-  #   address: "220 Kings St. West",
-  #   phone_number: "416-967-1111",
-  #   rest_image: "http://www.shawarmasking.ca/wp-content/uploads/2016/08/Shawarma-King-Logo2.png",
-  #   owner_id: 1
-  # )
+  rest = Restaurant.create!(
+    name: "Shawarma King",
+    address: "220 Kings St. West",
+    phone_number: "416-967-1111",
+    rest_img: nil,
+    # RestImgUploader.new.store!(File.open(File.join(Rails.root, '/public/images/ShawarmaKing2.jpeg'))),
+    # rest_img: open("http://www.shawarmasking.ca/wp-content/uploads/2016/08/Shawarma-King-Logo2.png"),
+    owner_id: 1
+  )
+  File.open(File.join(Rails.root, '/public/images/ShawarmaKing2.jpeg')) do |f|
+    rest.rest_img = f
+  end
+  rest.save!
+
+  rest = Restaurant.create!(
+    name: "Burgers Priest",
+    address: "212 Adelaide St. W",
+    phone_number: "416-416-4166",
+    rest_img: nil,
+    # RestImgUploader.new.store!(File.open(File.join(Rails.root, '/public/images/ShawarmaKing2.jpeg'))),
+    # rest_img: open("http://www.shawarmasking.ca/wp-content/uploads/2016/08/Shawarma-King-Logo2.png"),
+    owner_id: 1
+  )
+  File.open(File.join(Rails.root, '/public/images/BurgersPriest.jpeg')) do |f|
+    rest.rest_img = f
+  end
+  rest.save!
+
+  rest = Restaurant.create!(
+    name: "Burrito Boyz",
+    address: "226 Adelaide St. West",
+    phone_number: "416-555-1111",
+    rest_img: nil,
+    # RestImgUploader.new.store!(File.open(File.join(Rails.root, '/public/images/ShawarmaKing2.jpeg'))),
+    # rest_img: open("http://www.shawarmasking.ca/wp-content/uploads/2016/08/Shawarma-King-Logo2.png"),
+    owner_id: 1
+  )
+  File.open(File.join(Rails.root, '/public/images/BurritoBoyz.png')) do |f|
+    rest.rest_img = f
+  end
+  rest.save!
+
+  rest = Restaurant.create!(
+    name: "Smokes Poutine",
+    address: "224 Adelaide St. West",
+    phone_number: "416-444-4444",
+    rest_img: nil,
+    # RestImgUploader.new.store!(File.open(File.join(Rails.root, '/public/images/ShawarmaKing2.jpeg'))),
+    # rest_img: open("http://www.shawarmasking.ca/wp-content/uploads/2016/08/Shawarma-King-Logo2.png"),
+    owner_id: 1
+  )
+  File.open(File.join(Rails.root, '/public/images/SmokesPoutine.jpeg')) do |f|
+    rest.rest_img = f
+  end
+  rest.save!
+
+  rest = Restaurant.create!(
+    name: "Tokaji Winery and Restaurant",
+    address: "7887 Weston Rd.",
+    phone_number: "905-851-7877",
+    rest_img: nil,
+    # RestImgUploader.new.store!(File.open(File.join(Rails.root, '/public/images/ShawarmaKing2.jpeg'))),
+    # rest_img: open("http://www.shawarmasking.ca/wp-content/uploads/2016/08/Shawarma-King-Logo2.png"),
+    owner_id: 1
+  )
+  File.open(File.join(Rails.root, '/public/images/Tokaji.jpeg')) do |f|
+    rest.rest_img = f
+  end
+  rest.save!
   #
   # p = Restaurant.create!(
   #   name: Faker::App.name,
@@ -44,12 +105,14 @@
   #   owner_id: rand(5).ceil
   # )
   #
-  # 5.times do
-  #   p.rewards.create!(
-  #     name: Faker::Commerce.product_name,
-  #     description: Faker::Lorem.sentence,
-  #     point_value: rand(100)
-  #   )
+  5.times do
+    Reward.create!(
+      name: Faker::Commerce.product_name,
+      description: Faker::Lorem.sentence,
+      point_value: rand(100),
+      restaurant_id: rand(5)
+    )
+  end
 #
 # end
 
